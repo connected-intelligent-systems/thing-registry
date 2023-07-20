1399e088155284da1a3358344709a37fe3456ce4
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2023-07-20 19:58:38 +0000

    Add permission service

--- test/apis/permissions.js
+++ test/apis/permissions.js
@@ -34,92 +34,92 @@ describe('API', () => {
     })
 
     describe('Listing', () => {
-        let createdPermissionId;
+      let createdPermissionId
 
-        before(async () => {
-          await prisma.thing.deleteMany({})
-          await prisma.permission.deleteMany({})
-          await queries.createThing({
-            owner: '5ccc7bff-7e60-4604-ac1e-c1b7b0b5951d',
-            description: tdValid1,
-            enableProxy: false
-          })
-         const createPermission = await prisma.permission.create({
-            data: {
-                entityId: "unique-user-id",
+      before(async () => {
+        await prisma.thing.deleteMany({})
+        await prisma.permission.deleteMany({})
+        await queries.createThing({
+          owner: '5ccc7bff-7e60-4604-ac1e-c1b7b0b5951d',
+          description: tdValid1,
+          enableProxy: false
+        })
+        const createPermission = await prisma.permission.create({
+          data: {
+            entityId: 'unique-user-id',
+            entityType: 'user',
+            scope: 'read',
+            thingId: 'urn:dev:ops:32473-WoTLamp-1234'
+          }
+        })
+        createdPermissionId = createPermission.id
+      })
+
+      it('get created permissions', done => {
+        chai
+          .request(app)
+          .get('/registry/permissions')
+          .set('authorization', `Bearer ${accessTokenUser1}`)
+          .end((_, res) => {
+            res.should.have.status(200)
+            res.body.should.to.have.lengthOf(1)
+            res.body.should.to.have.deep.members([
+              {
+                entityId: 'unique-user-id',
                 entityType: 'user',
+                id: res.body[0].id,
                 scope: 'read',
                 thingId: 'urn:dev:ops:32473-WoTLamp-1234'
-            }
-          })
-          createdPermissionId = createPermission.id
-        })
-  
-        it('get created permissions', done => {
-          chai
-            .request(app)
-            .get('/registry/permissions')
-            .set('authorization', `Bearer ${accessTokenUser1}`)
-            .end((_, res) => {
-              res.should.have.status(200)
-              res.body.should.to.have.lengthOf(1)
-              res.body.should.to.have.deep.members([
-                {
-                    "entityId": "unique-user-id",
-                    "entityType": "user",
-                    "id": res.body[0].id,
-                    "scope": "read",
-                    "thingId": "urn:dev:ops:32473-WoTLamp-1234"
-                  }
-              ])
-              done()
-            })
-        })
-
-        it('get permission for user 2', done => {
-            chai
-              .request(app)
-              .get('/registry/permissions')
-              .set('authorization', `Bearer ${accessTokenUser2}`)
-              .end((_, res) => {
-                res.should.have.status(200)
-                res.body.should.be.eql([])
-                done()
-              })
-          })
-
-          it('delete permission with user 2', done => {
-            chai
-              .request(app)
-              .delete(`/registry/permissions/${createdPermissionId}`)
-              .set('authorization', `Bearer ${accessTokenUser2}`)
-              .end((_, res) => {
-                res.should.have.status(404)
-                done()
-              })
-          })
-
-          it('delete permission with user 1', done => {
-            chai
-              .request(app)
-              .delete(`/registry/permissions/${createdPermissionId}`)
-              .set('authorization', `Bearer ${accessTokenUser1}`)
-              .end((_, res) => {
-                res.should.have.status(200)
-                done()
-              })
-          })
-
-          it('delete non-existing permission', done => {
-            chai
-              .request(app)
-              .delete(`/registry/permissions/${createdPermissionId}`)
-              .set('authorization', `Bearer ${accessTokenUser1}`)
-              .end((_, res) => {
-                res.should.have.status(404)
-                done()
-              })
+              }
+            ])
+            done()
           })
       })
+
+      it('get permission for user 2', done => {
+        chai
+          .request(app)
+          .get('/registry/permissions')
+          .set('authorization', `Bearer ${accessTokenUser2}`)
+          .end((_, res) => {
+            res.should.have.status(200)
+            res.body.should.be.eql([])
+            done()
+          })
+      })
+
+      it('delete permission with user 2', done => {
+        chai
+          .request(app)
+          .delete(`/registry/permissions/${createdPermissionId}`)
+          .set('authorization', `Bearer ${accessTokenUser2}`)
+          .end((_, res) => {
+            res.should.have.status(404)
+            done()
+          })
+      })
+
+      it('delete permission with user 1', done => {
+        chai
+          .request(app)
+          .delete(`/registry/permissions/${createdPermissionId}`)
+          .set('authorization', `Bearer ${accessTokenUser1}`)
+          .end((_, res) => {
+            res.should.have.status(200)
+            done()
+          })
+      })
+
+      it('delete non-existing permission', done => {
+        chai
+          .request(app)
+          .delete(`/registry/permissions/${createdPermissionId}`)
+          .set('authorization', `Bearer ${accessTokenUser1}`)
+          .end((_, res) => {
+            res.should.have.status(404)
+            done()
+          })
+      })
+    })
   })
 })
