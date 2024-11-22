4d246b09a6a7efe2cf4b1698a46f8c69e9da5755
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2024-11-22 09:11:28 +0000

    Check return code of migration

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "$NODE_ENV" = "development" ]; then
-    npm run migrate:development
+    npm run migrate:development  || { echo "Migration failed"; exit 1; }
     exec npm run start:development
 else
-    npm run migrate:production
+    npm run migrate:production || { echo "Migration failed"; exit 1; }
     exec npm start
 fi
